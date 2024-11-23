4c3b6879591418af096dcb92c78a5b9483256267
Anonymous <anonymous@example.com>
2024-11-23 22:13:27 +0800

    feat: Retrieve the tokens using a Scanner object

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
+import java.util.List;
+
 public class Main { 
     public static void main(String[] args) {
+        // Initialize a Scanner
+        List tokens = new Scanner().getTokens();
+
         // Initialize Parser
         Parser parser = new Parser();
     }
