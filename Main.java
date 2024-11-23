9e40ec510517a3ec40e0c51d59754c490003613b
Anonymous <anonymous@example.com>
2024-11-23 22:22:25 +0800

    fix: Change the data type declaration to reflect 2D array of tokens

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ import java.util.List;
 public class Main { 
     public static void main(String[] args) {
         // Initialize a Scanner
-        List tokens = new Scanner().getTokens();
+        List <List<Token>> tokens = new Scanner().getTokens();
 
         // Initialize Parser
         Parser parser = new Parser();
