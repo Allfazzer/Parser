501fbe04112b65d7dc2769fb2f1bf1bbe8c67f5a
Anonymous <anonymous@example.com>
2024-11-23 22:25:14 +0800

    feat: Split the initialization and retrieval of tokens into separate lines for readability

--- Main.java
+++ Main.java
@@ -3,7 +3,10 @@ import java.util.List;
 public class Main { 
     public static void main(String[] args) {
         // Initialize a Scanner
-        List <List<Token>> tokens = new Scanner().getTokens();
+        Scanner scanner =  new Scanner();
+
+        // Retrieve the tokens from the scanner
+        List <List<Token>> tokens = scanner.getTokens();
 
         // Initialize Parser
         Parser parser = new Parser(tokens);
