0d6431ce90bd8f5368f88b42e613a68f16b708f5
ax3nrd <91680249+ax3nrd@users.noreply.github.com>
2024-12-04 11:20:04 +0800

    Update Parser.java

--- Parser.java
+++ Parser.java
@@ -2,9 +2,26 @@ import java.util.List;
 
 public class Parser {
     private List<List<Token>> tokens;  
+    private Scanner scanner
 
     // Constructor that initializes the 2D tokens list
     public Parser(List<List<Token>> tokens) {
         this.tokens = tokens; 
+    }
+
+    public ParseTreeNode parseSentence(){
+        tokens = scanner.nextToken();
+    }
+
+    private ParseTreeNode parseComplexSentence(){
+        //insert nested if-else statement to check 
+
+        //ParseTreeNode left = parseAtomicSentence();
+    }
+
+    private ParseTreeNode parseAtomicSentence(){
+        //insert nested if-else statement to check 
+
+        //return node;
     }
 }
